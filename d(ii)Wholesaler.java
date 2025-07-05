6c323e3db838d3dbd1678a984a8e9c9c930e934a
AdrianPhoenix35 <magamboaddriane@gmail.com>
2025-07-05 15:18:49 +0300

    Create d(ii)Wholesaler.java

---
+++ d(ii)Wholesaler.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package adrian.main1;
+
+/**
+ *
+ * @author ADRIAN M
+ */
+public class Wholesaler extends TaxCategory {
+    @Override
+    public double calculateVAT(double amount) {
+        return amount * 0.15;
+    }
+}
+
+    
+
